191e1b25d8f98a4ccbe6c0323e87b92ac62386d2
Alyssa De Los Santos <adelossantos6@gmail.com>
2023-07-01 09:46:02 -0500

    completes functions continued

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(fun = 'roller-skate') {
+    return `This Saturday, I want to ${fun}!`
+}
+
+const mondayWork = function (toDo = "go to the office") {
+    return `This Monday, I will ${toDo}.`
+}
+
+function wrapAdjective(visualFlare = "*") {
+    return function (adjective = "special") {
+        return `You are ${visualFlare}${adjective}${visualFlare}!`
+    }
+}
